0708cb79184579aa7d2c8a0266674563a157aae5
tuc.nguyen <tuc.nguyen@logigear.com>
2018-01-30 17:15:27 +0700

    Commit sample GetExchangeRates

---
+++ LinearRegression/LinearRegression/ExchangeRates.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace LinearRegression
+{
+    public class ExchangeRates
+    {
+        public string Base;
+        public DateTime Date;
+        public Dictionary<string, float> Rates;
+
+        public void DeserializeFromJson(string input)
+        {
+            ExchangeRates data = JsonConvert.DeserializeObject<ExchangeRates>(input);
+            this.Base = data.Base;
+            this.Date = data.Date;
+            this.Rates = data.Rates;
+        }
+    }
+}
--- LinearRegression/LinearRegression/Program.cs
+++ LinearRegression/LinearRegression/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +10,48 @@ namespace LinearRegression
 {
     class Program
     {
+        public const string apiLink = "https://api.fixer.io/";
+        public const string defaultDataType = "application/json";
+        public const string defaultFromCurrency = "USD";
+
         static void Main(string[] args)
         {
+            Test();
+        }
+
+        static async void Test()
+        {
+            DateTime date = new DateTime(2018, 1, 15);
+            ExchangeRates exchangeRates = await GetExchangeRates(defaultFromCurrency, date);
+        }
+
+        public static async Task<ExchangeRates> GetExchangeRates(string fromCurrency, DateTime date)
+        {
+            ExchangeRates res = new ExchangeRates();
+
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    client.DefaultRequestHeaders.Accept.Add(
+                        new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue(defaultDataType));
+
+                    using (HttpResponseMessage response = client.GetAsync(
+                       apiLink + "/" + date.ToString("yyyy-MM-dd") + "?base=" + fromCurrency).Result)
+                    {
+                        response.EnsureSuccessStatusCode();
+                        string responseBody = await response.Content.ReadAsStringAsync();
+                        Console.WriteLine(responseBody);
+                        res.DeserializeFromJson(responseBody);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+            return res;
         }
     }
 }
